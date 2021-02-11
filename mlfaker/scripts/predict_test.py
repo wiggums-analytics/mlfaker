e013f0e547a611b824ec5eb5fcb5933cd62eca16
Michael Stefferson <mstefferson@gmail.com>
2021-02-11 15:10:41 -0400

    Clean-up

--- mlfaker/scripts/predict_test.py
+++
@@ -1,9 +0,0 @@
-from click.testing import CliRunner
-
-from mlfaker.scripts.predict import main
-
-
-def test_predict():
-    runner = CliRunner()
-    result = runner.invoke(main, [])
-    assert result.exit_code == 0
